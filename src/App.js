d70369f5f8d68702d1a97786ab23f41b63d74dd3
datomaluta <datomaluta@gmail.com>
2022-08-05 18:55:56 +0400

    netlify refresh bug check

--- src/App.js
+++ src/App.js
@@ -115,7 +115,9 @@ function App() {
       <Route path="/" exact>
         <Redirect to="/inplay" />
       </Route>
-      <Route path="/inplay"></Route>
+      <Route path="/inplay">
+        <Redirect to="/" />
+      </Route>
 
       {/* 
 
